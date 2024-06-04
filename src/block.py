634a000d3b5a6bd25584da819ee02162d235098c
Patrick Ruppert <patrick.j.ruppert@gmail.com>
2024-06-04 13:11:35 -0400

    Ch4.2 Block types

--- src/block.py
+++ src/block.py
@@ -1,4 +1,14 @@
 from typing import List
+from enum import Enum
+
+
+class BlockType(Enum):
+    Paragraph = 1
+    Heading = 2
+    Code = 3
+    Quote = 4
+    UnorderedList = 5
+    OrderedList = 6
 
 
 def markdown_to_blocks(text: str) -> List[str]:
@@ -14,3 +24,59 @@ def markdown_to_blocks(text: str) -> List[str]:
             )
         )
     )
+
+
+def block_to_block_type(text: str) -> BlockType:
+    if type(text) is not str:
+        raise ValueError("text must be a string")
+
+    def is_heading():
+        if len(text.split("\n")) > 1:
+            return False
+        for i in range(1, 7):
+            line_beginning = "#" * i + " "
+            remaining_text = text.replace(line_beginning, "", 1)
+            if (
+                text.startswith(line_beginning)
+                and len(remaining_text) > 0
+                and not remaining_text.isspace()
+            ):
+                return True
+        return False
+
+    def is_code():
+        return text.startswith("```") and text.endswith("```")
+
+    def is_quote():
+        lines = text.split("\n")
+        for line in lines:
+            if not line.startswith("> "):
+                return False
+        return True
+
+    def is_unordered_list():
+        lines = text.split("\n")
+        for line in lines:
+            if not line.startswith("* ") and not line.startswith("- "):
+                return False
+        return True
+
+    def is_ordered_list():
+        lines = text.split("\n")
+        for i in range(len(lines)):
+            if not lines[i].startswith(f"{i + 1}. "):
+                return False
+        return True
+
+    if is_heading():
+        return BlockType.Heading
+    elif is_code():
+        return BlockType.Code
+    elif is_quote():
+        return BlockType.Quote
+    elif is_unordered_list():
+        return BlockType.UnorderedList
+    elif is_ordered_list():
+        return BlockType.OrderedList
+    else:
+        return BlockType.Paragraph
--- src/test_block.py
+++ src/test_block.py
@@ -1,6 +1,6 @@
 import unittest
 
-from block import markdown_to_blocks
+from block import markdown_to_blocks, BlockType, block_to_block_type
 
 
 class TestMarkdownToBlocks(unittest.TestCase):
@@ -40,4 +40,125 @@ class TestMarkdownToBlocks(unittest.TestCase):
 
              "* This is a list\n* with items"],
             markdown_to_blocks(self.example_text)
+        )
+
+
+class TestBlockTypes(unittest.TestCase):
+    def test_block_types_exist(self):
+        for block_type in [
+            'Paragraph',
+            'Heading',
+            'Code',
+            'Quote',
+            'UnorderedList',
+            'OrderedList',
+        ]:
+            self.assertTrue(block_type in BlockType._member_names_)
+
+
+class TestBlockToBlockType(unittest.TestCase):
+
+    def test_requires_string_as_input(self):
+        with self.assertRaises(ValueError):
+            block_to_block_type([])
+
+    def test_parses_paragraphs(self):
+        for block in [
+            "Just a simple paragraph",
+            "This paragraph\nis a multiline paragraph",
+        ]:
+            self.assertEqual(
+                BlockType.Paragraph,
+                block_to_block_type(block)
+            )
+
+    def test_parses_headings(self):
+        self.assertListEqual(
+            [
+                BlockType.Heading,
+                BlockType.Heading,
+                BlockType.Heading,
+                BlockType.Heading,
+                BlockType.Heading,
+                BlockType.Heading,
+                BlockType.Paragraph,
+                BlockType.Paragraph,
+                BlockType.Paragraph,
+            ],
+            list(map(block_to_block_type, [
+                "# Level 1 Heading",
+                "## Level 2 Heading",
+                "### Level 3 Heading",
+                "#### Level 4 Heading",
+                "##### Level 5 Heading",
+                "###### Level 6 Heading",
+                "####### Invalid Heading Level",
+                "#Missing space after #",
+                "### "  # No text after marker
+            ])))
+
+    def test_parses_code(self):
+        self.assertListEqual(
+            [
+                BlockType.Code,
+                BlockType.Code,
+                BlockType.Paragraph,
+                BlockType.Paragraph,
+                BlockType.Paragraph,
+            ],
+            list(map(block_to_block_type, [
+                "```\nThis is a code block\n```",
+                "```\nThis is a multiline\nCode block```",
+                "``Only two backticks``",
+                "```Missing ending backticks",
+                "Missing starting backticks```"
+            ]))
+        )
+
+    def test_parses_quotes(self):
+        self.assertListEqual(
+            [
+                BlockType.Quote,
+                BlockType.Quote,
+                BlockType.Paragraph,
+                BlockType.Paragraph,
+                BlockType.Paragraph,
+            ],
+            list(map(block_to_block_type, [
+                "> This is a quote block",
+                "> This is a multiline\n> Quote block",
+                ">Missing space",
+                ">\nNewline instead of space",
+                ">\tTab instead of space",
+            ]))
+        )
+
+    def test_parses_unordered_lists(self):
+        self.assertListEqual(
+            [
+                BlockType.UnorderedList,
+                BlockType.UnorderedList,
+                BlockType.Paragraph,
+                BlockType.Paragraph,
+            ],
+            list(map(block_to_block_type, [
+                "* Unordered item 1\n* Unordered item 2",
+                "- Slash list 1\n- Slash list 2",
+                "* Missing second\nasterisk",
+                "Missing first\n- slash",
+            ]))
+        )
+
+    def test_parses_ordered_lists(self):
+        self.assertListEqual(
+            [
+                BlockType.OrderedList,
+                BlockType.Paragraph,
+                BlockType.Paragraph,
+            ],
+            list(map(block_to_block_type, [
+                "1. Ordered item 1\n2. Ordered item 2\n3. Ordered item 3",
+                "1. Ordered item\nMissing prefix",
+                "1. Ordered item\n3. Skipped sequence",
+            ]))
         )
